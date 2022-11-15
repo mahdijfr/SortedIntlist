89dd497b6d2189423a777c13a0c8439659de3177
mahdijfr <mahdijafarinasr@gmail.com>
2022-11-15 23:12:27 +0330

    Initial commit

--- SortedIntlist/src/SortedIntList.java
+++ SortedIntlist/src/SortedIntList.java
@@ -1,25 +1,22 @@
 public class SortedIntList extends IntList {
-
-    int temp=0;
     public SortedIntList(int size) {
         super(size);
     }
 
+    @Override
     public void add(int value) {
-        if (numElements == list.length) System.out.println(
-                "Can't add, list is full"
-        ); else {
-                for (int i = 0; i <numElements; i++) {
-                    for (int j = 0; j < numElements - i - 1; j++) {
-                        if (list[j + 1] < list[j]) {
-                            temp = list[j + 1];
-                            list[j + 1] = list[j];
-                            list[j] = temp;
-                        }
-                    }
-                }
-            list[numElements] = value;
-            numElements++;
+        if (numElements == list.length) {
+            System.out.println("Can't add, list is full");
+            return;
         }
+        for (int i = 0; i < numElements; i++) {
+            if (list[i] > value) {
+                int temp = list[i];
+                list[i] = value;
+                value = temp;
+            }
+        }
+        list[numElements] = value;
+        numElements++;
     }
 }
